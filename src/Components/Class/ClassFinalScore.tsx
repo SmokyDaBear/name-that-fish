356a8bc559a72b0ed56955b17c45dc154788ce12
SmokyDaBear <jeshurungreen@gmail.com>
2025-10-19 23:50:11 -0500

    added "reset Game" functions

--- src/Components/Class/ClassFinalScore.tsx
+++ src/Components/Class/ClassFinalScore.tsx
@@ -3,10 +3,11 @@ import { Component } from "react";
 type TClassFinalScoreProps = {
   correctCount: number;
   incorrectCount: number;
+  resetGame: () => void;
 };
 export class ClassFinalScore extends Component<TClassFinalScoreProps> {
   render() {
-    const { correctCount, incorrectCount } = this.props;
+    const { correctCount, incorrectCount, resetGame } = this.props;
     const totalCount = correctCount + incorrectCount;
     return (
       <div id="final-score">
@@ -15,6 +16,7 @@ export class ClassFinalScore extends Component<TClassFinalScoreProps> {
           <p>{correctCount}</p>
           <hr />
           <p>{totalCount}</p>
+          <button onClick={resetGame}>Play Again</button>
         </div>
       </div>
     );
--- src/Components/Functional/FunctionalFinalScore.tsx
+++ src/Components/Functional/FunctionalFinalScore.tsx
@@ -3,10 +3,11 @@ import "./styles/final-score.css";
 type TFunctionFinalScoreProps = {
   correctCount: number;
   incorrectCount: number;
+  resetGame: () => void;
 };
 
 export function FunctionalFinalScore(props: TFunctionFinalScoreProps) {
-  const { correctCount, incorrectCount } = props;
+  const { correctCount, incorrectCount, resetGame } = props;
   const totalCount = correctCount + incorrectCount;
   return (
     <div id="final-score">
@@ -15,6 +16,7 @@ export function FunctionalFinalScore(props: TFunctionFinalScoreProps) {
         <p>{correctCount}</p>
         <hr />
         <p>{totalCount}</p>
+        <button onClick={resetGame}>Play Again</button>
       </div>
     </div>
   );
